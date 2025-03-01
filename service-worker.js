9290002cdb97debe1960a9c8f00ad8da7fbc0c55
Rohit Kumar <121348164+rohit7nkuamr@users.noreply.github.com>
2025-03-01 17:16:42 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blaze-svg-cache-v1';
+const CACHE_NAME = 'blaze-cache-v2'; // Increment cache version
 const urlsToCache = [
     '/',
     '/index.html',
@@ -14,6 +14,23 @@ self.addEventListener('install', event => {
             return cache.addAll(urlsToCache);
         })
     );
+    self.skipWaiting(); // Immediately activate new service worker
+});
+
+// Delete old caches when a new service worker is activated
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); // Take control of open pages immediately
 });
 
 self.addEventListener('fetch', event => {
