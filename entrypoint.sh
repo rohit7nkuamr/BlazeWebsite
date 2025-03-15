2d3d5d3f22649bef4c2eb65e632f906c37e62761
Rohit Kumar <rohit7ngod@gmail.com>
2025-03-15 23:07:48 +0530

    Fix PORT environment variable handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@
 # Default to port 8000 if PORT not set
 PORT="${PORT:-8000}"
 
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
 # Start gunicorn
 exec gunicorn blaze.wsgi:application --bind 0.0.0.0:$PORT
